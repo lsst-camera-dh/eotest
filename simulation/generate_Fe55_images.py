314371813e93e9599241783b5e8eb1bb043949b1
Jim Chiang <jchiang@slac.stanford.edu>
2013-02-05 09:15:29 -0800

    recast as functions

--- simulation/generate_Fe55_images.py
+++ simulation/generate_Fe55_images.py
@@ -4,48 +4,58 @@ according to section 5.4 of the E/O document (Dec 19, 2012 version).
 
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
+import os
 import numpy as np
 from sim_inputs import *
 
-nexp = 10
+def generate_Fe55_images(exptimes, nxrays, outdir, sensorid):
+    nexp = len(exptimes)
+    for i, exptime, nxray in zip(range(nexp), exptimes, nxrays):
+        #
+        # Bias images
+        #
+        outfile = "Fe55_bias_%s_%02i.fits" % (sensorid, i)
+        bias_file = os.path.join(outdir, outfile)
+        bias_segs = []
+        for hdu in range(nhdu):
+            seg = SegmentExposure(exptime=0, gain=gain)
+            seg.add_bias(level=level, sigma=sys_noise) # electronics
+            seg.add_bias(sigma=sigma, level=0) # read noise
+            bias_segs.append(seg)
+        writeFits(bias_segs, bias_file)
+        #
+        # Dark images
+        #
+        outfile = "Fe55_dark_%s_%02i.fits" % (sensorid, i)
+        dark_file = os.path.join(outdir, outfile)
+        dark_segs = []
+        for hdu in range(nhdu):
+            seg = SegmentExposure(exptime=exptime, gain=gain)
+            seg.add_bias(level=level, sigma=sys_noise) # electronics
+            seg.add_bias(level=0, sigma=sigma) # read noise
+            seg.add_dark_current(level=200) # dark current
+            dark_segs.append(seg)
+        writeFits(dark_segs, dark_file)
+        #
+        # Fe55 exposures
+        #
+        outfile = "Fe55_exp_%s_%02i.fits" % (sensorid, i)
+        Fe55_file = os.path.join(outdir, outfile)
+        fe55_segs = []
+        for hdu in range(nhdu):
+            seg = SegmentExposure(exptime=exptime, gain=gain)
+            seg.add_bias(level=level, sigma=sys_noise) # electronics
+            seg.add_bias(level=0, sigma=sigma) # read noise
+            seg.add_dark_current(level=200) # dark current
+            seg.add_Fe55_hits(nxrays=nxray)
+            fe55_segs.append(seg)
+        writeFits(fe55_segs, Fe55_file)
 
-exptimes = np.linspace(1, 5, nexp)
-nxrays = [int(x*1000) for x in exptimes] 
+if __name__ == '__main__':
+    nexp = 10
 
-for i, exptime, nxray in zip(range(nexp), exptimes, nxrays):
-    #
-    # Bias images
-    #
-    bias_file = "Fe55_bias_%02i.fits" % i
-    bias_segs = []
-    for hdu in range(nhdu):
-        seg = SegmentExposure(exptime=0, gain=gain)
-        seg.add_bias(level=level, sigma=sys_noise) # electronics
-        seg.add_bias(sigma=sigma, level=0) # read noise
-        bias_segs.append(seg)
-    writeFits(bias_segs, bias_file)
-    #
-    # Dark images
-    #
-    dark_file = "Fe55_dark_%02i.fits" % i
-    dark_segs = []
-    for hdu in range(nhdu):
-        seg = SegmentExposure(exptime=exptime, gain=gain)
-        seg.add_bias(level=level, sigma=sys_noise) # electronics
-        seg.add_bias(level=0, sigma=sigma) # read noise
-        seg.add_dark_current(level=200) # dark current
-        dark_segs.append(seg)
-    writeFits(dark_segs, dark_file)
-    #
-    # Fe55 exposures
-    #
-    Fe55_file = "Fe55_exp_%02i.fits" % i
-    fe55_segs = []
-    for hdu in range(nhdu):
-        seg = SegmentExposure(exptime=exptime, gain=gain)
-        seg.add_bias(level=level, sigma=sys_noise) # electronics
-        seg.add_bias(level=0, sigma=sigma) # read noise
-        seg.add_dark_current(level=200) # dark current
-        seg.add_Fe55_hits(nxrays=nxray)
-        fe55_segs.append(seg)
-    writeFits(fe55_segs, Fe55_file)
+    exptimes = np.linspace(1, 5, nexp)
+    nxrays = [int(x*1000) for x in exptimes] 
+
+    generate_Fe55_images(exptimes, nxrays, '.', 'xxx-xx')
+
---
+++ simulation/generate_readout_noise_images.py
@@ -0,0 +1,25 @@
+"""
+@brief Generate the readout system noise frames according to section
+5.8 of the E/O document (Dec 19, 2012 version).
+
+@author J. Chiang <jchiang@slac.stanford.edu>
+"""
+import os
+import numpy as np
+from sim_inputs import *
+
+def generate_readout_noise_images(nexp, outdir, sensorid):
+    exptime = 0
+    for i in range(nexp):
+        filename = 'readout_noise_%s_%02i.fits' % (sensorid, i)
+        outfile = os.path.join(outdir, filename)
+        noise_segs = []
+        for hdu in range(nhdu):
+            seg = SegmentExposure(exptime=exptime, gain=gain)
+            seg.add_bias(level=level, sigma=sys_noise)
+            noise_segs.append(seg)
+        writeFits(noise_segs, outfile)
+
+if __name__ == '__main__':
+    nexp = 10
+    generate_readout_noise_images(nexp, '.')
