d187a724531e8d95d5ed4040c1074a68135ad84c
Jim Chiang <jchiang@slac.stanford.edu>
2013-02-12 11:27:15 -0800

    add database subdir and code to handle mysql tables

---
+++ database/MySQL_Database.py
@@ -0,0 +1,53 @@
+"""
+@brief Database interface class using MySQL.
+
+@author J. Chiang
+"""
+import os
+import MySQLdb
+from Parfile import Parfile
+
+db2_imp = MySQLdb
+
+mysql_db_data = 'mysql_db_data.par'
+
+def nullFunc(*args):
+    return None
+
+class Database(object):
+    def __init__(self, dbdata=mysql_db_data):
+        self.pars = Parfile(dbdata)
+    def apply(self, sql, args=None, cursorFunc=nullFunc):
+        sql = sql.replace('?', '%s')
+        my_connection = MySQLdb.connect(**self.pars)
+        cursor = my_connection.cursor()
+        try:
+            if args is None:
+                cursor.execute(sql)
+            else:
+                cursor.execute(sql, args)
+            results = cursorFunc(cursor)
+        except db2_imp.DatabaseError, message:
+            cursor.close()
+            my_connection.close()
+            raise db2_imp.DatabaseError, message
+        cursor.close()
+        if cursorFunc is nullFunc:
+            my_connection.commit()
+        my_connection.close()
+        return results
+
+def getDbObjects(tablename, dbdata=mysql_db_data):
+    """Return a list of entries for the specified db table"""
+    sql = "SELECT * from %s" % tablename
+    db = Database(dbdata)
+    def cursorFunc(cursor):
+        cols = [column[0] for column in cursor.description]
+        entries = []
+        for item in cursor:
+            entries.append(dict(zip(cols, item)))
+        return entries
+    return db.apply(sql, cursorFunc=cursorFunc)
+
+if __name__ == '__main__':
+    pass
---
+++ database/SensorDb.py
@@ -0,0 +1,68 @@
+"""
+@brief Classes for entering sensor test results into database tables.
+
+@author J. Chiang <jchiang@slac.stanford.edu>
+"""
+from MySQL_Database import Database
+
+class Sensor(object):
+    def __init__(self, ccdId, sensorDb):
+        self.ccdId = ccdId
+        self.db = sensorDb
+    def add_ccd_result(self, column, value):
+        sql = ("update CCD set %s=%s where id=%i"
+               % (column, value, self.ccdId))
+        self.db.apply(sql)
+    def add_seg_result(self, segment, column, value):
+        sql = ("""update Segment set %s=%s where
+               ccdId=%i and channelId='%02i'"""
+               % (column, value, self.ccdId, segment))
+        self.db.apply(sql)
+
+_default_callback = lambda curs : [x[0] for x in curs][0]
+
+class SensorDbException(Exception):
+    def __init__(self, *args):
+        Exception.__init__(self, *args)
+
+class SensorDb(Database):
+    def __init__(self, dbdata):
+        Database.__init__(self, dbdata)
+    def getSensor(self, vendor, vendorId):
+        try:
+            return self.addSensor(vendor, vendorId)
+        except SensorDbException:
+            sql = """select ccdId from CCD_VendorIds
+                  where vendor='%(vendor)s' and
+                  vendorId='%(vendorId)s'""" % locals()
+            return Sensor(self.apply(sql, cursorFunc=_default_callback), self)
+    def addSensor(self, vendor, vendorId):
+        sql = """select ccdId from CCD_VendorIds where vendor='%(vendor)s'
+              and vendorId='%(vendorId)s'""" % locals()
+        try:
+            ccdId = self.apply(sql, cursorFunc=_default_callback)
+            raise SensorDbException("Sensor already in db")
+        except IndexError:
+            pass
+        sql = "insert into CCD () values ();"
+        self.apply(sql)
+        sql = "select id from CCD order by id desc"
+        ccdId = self.apply(sql, cursorFunc=_default_callback)
+        sql = """insert into CCD_VendorIds (ccdId, vendor, vendorId) values
+                 (%(ccdId)i, '%(vendor)s', '%(vendorId)s')""" % locals()
+        self.apply(sql)
+        for segment in range(18):
+            channelId = "%02i" % segment
+            sql = """insert into Segment (channelId, ccdId) values
+                     ('%(channelId)s', %(ccdId)i)""" % locals()
+            self.apply(sql)
+        print "Adding new sensor entry for", vendor, vendorId
+        return Sensor(ccdId, self)
+ 
+if __name__ == '__main__':
+    sensorDb = SensorDb('mysql_db_data.par')
+    vendor = 'e2v'
+    vendorId = '000-00'
+    my_sensor = sensorDb.getSensor(vendor, vendorId)
+    my_sensor.add_ccd_result('ctiSerialMean', 5e-6)
+    my_sensor.add_seg_result(0, 'ctiSerial', 3e-6)
