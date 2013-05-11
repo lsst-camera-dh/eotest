6cd3024a5b877be34b89a3da92af50ab4ecd1cbc
Jim Chiang <jchiang@slac.stanford.edu>
2013-05-11 13:42:21 -0700

    add visualization

--- crosstalk.py
+++ crosstalk.py
@@ -1,4 +1,11 @@
+"""
+@brief Calculations for system and detector crosstalk inputs.
+
+@author J. Chiang <jchiang@slac.stanford.edu>
+"""
+import os
 import numpy as np
+import pylab
 import lsst.afw.detection as afwDetect
 import lsst.afw.geom as afwGeom
 import lsst.afw.math as afwMath
@@ -17,7 +24,9 @@ def system_crosstalk(imfile, aggressor_amp, ethresh=10000,
                      mask_files=()):
     """
     Compute the system crosstalk.  ethresh is the threshold in DN for
-    detecting the illuminated column in the aggressor amplifier.
+    detecting the illuminated column in the aggressor amplifier.  This
+    is optimized for system crosstalk input, i.e., for which the
+    aggressor amp has a single illuminated column.
     """
     #
     # Read in the system cross-talk images.
@@ -98,11 +107,10 @@ def detector_crosstalk(imfile, aggressor_amp, ethresh=10000,
     return ratios
 
 class CrosstalkMatrix(object):
-    def __init__(self, filename=None, mode='w', namps=16):
+    def __init__(self, filename=None, namps=16):
         self.filename = filename
-        self.mode = mode
         self.namps = namps
-        if mode == 'r':
+        if self.filename is not None and os.path.isfile(self.filename):
             self._read_matrix()
         else:
             self._set_matrix()
@@ -123,16 +131,49 @@ class CrosstalkMatrix(object):
     def write(self, outfile=None):
         if outfile is None:
             outfile = self.filename
+        else:
+            self.filename = outfile
         output = open(outfile, 'w')
         output.write('#')
         for amp in range(1, self.namps+1, 1):
-            output.write('victim%02i/aggressor  ' % amp)
+            output.write('%02i  ' % amp)
         output.write('\n')
         for agg in range(self.namps):
             for victim in range(self.namps):
                 output.write('%12.4e  ' % self.matrix[agg][victim])
             output.write('\n')
         output.close()
+    def plot_matrix(self, title=None, cmap_range=(0.6, 0.4), precision=3, 
+                    scale_factor=1e2, fontsize=10, figsize=(10, 6)):
+        cmin, cmax = cmap_range
+        my_matrix = np.copy(self.matrix)*scale_factor
+        for i in range(self.namps):
+            my_matrix[i][i] = 0
+        thresh = 10**(-precision)
+        cdict = dict(red=((0, cmin, cmin), (1, cmax, cmax)),
+                     green=((0, cmin, cmin), (1, cmax, cmax)),
+                     blue=((0, cmin, cmin), (1, cmax, cmax)))
+        unsat_grey = pylab.matplotlib.colors.LinearSegmentedColormap('my_grey', 
+                                                                     cdict, 256)
+        fig = pylab.figure(figsize=figsize)
+        axes = fig.add_subplot(111)
+        pylab.imshow(np.abs(my_matrix), interpolation='nearest',
+                     cmap=unsat_grey, aspect='auto')
+        axes.set_xticks(range(self.namps))
+        axes.set_yticks(range(self.namps))
+        axes.set_xticklabels(['%i' % i for i in range(1, self.namps+1)])
+        axes.set_yticklabels(['%i' % i for i in range(1, self.namps+1)])
+        if title is not None:
+            axes.set_title(title)
+        ny, nx = my_matrix.shape
+        for x in range(nx):
+            for y in range(ny):
+                if np.abs(my_matrix[y][x]) < thresh:
+                    label = '0'
+                else:
+                    label = '%.3f' % my_matrix[y][x]
+                pylab.text(x, y, label, horizontalalignment='center', 
+                           fontsize=fontsize)
     def __sub__(self, other):
         result = CrosstalkMatrix()
         result.matrix = self.matrix - other.matrix
@@ -144,22 +185,35 @@ class CrosstalkMatrix(object):
 
 if __name__ == '__main__':
     sys_xtfile = lambda amp : '/nfs/farm/g/lsst/u1/testData/eotestData/System/xtalk/data/xtalk_seg%02i.fits' % amp
-    #
-    # Loop over aggressor amps.
-    #
-    sys_xtalk = CrosstalkMatrix()
-    det_xtalk = CrosstalkMatrix()
-    for agg_amp in imutils.allAmps:
-        ratios = system_crosstalk(sys_xtfile(agg_amp), agg_amp)
-        sys_xtalk.set_row(agg_amp, ratios)
+#    #
+#    # System crosstalk calculation
+#    #
+#    sys_xtalk = CrosstalkMatrix()
+#    #
+#    # Loop over aggressor amps.
+#    #
+#    for agg_amp in imutils.allAmps:
+#        ratios = system_crosstalk(sys_xtfile(agg_amp), agg_amp)
+#        sys_xtalk.set_row(agg_amp, ratios)
+#    sys_xtalk.write('sys_xtalk.txt')
+#
+#    #
+#    # Read it back in from the text file and plot.
+#    #
+    foo = CrosstalkMatrix('sys_xtalk.txt')
+    foo.plot_matrix('System crosstalk')
 
-        det_ratios = detector_crosstalk(sys_xtfile(agg_amp), agg_amp)
-        det_xtalk.set_row(agg_amp, det_ratios)
-
-    print sys_xtalk.matrix
-    print
-    print det_xtalk.matrix
-    print
-
-    diff_xtalk = det_xtalk - sys_xtalk
-    print diff_xtalk.matrix
+#    #
+#    # Compute detector crosstalk from spot image datasets. (Use
+#    # system file as proxy.)
+#    #
+#    det_xtalk = CrosstalkMatrix()
+#    for agg_amp in imutils.allAmps:
+#        det_ratios = detector_crosstalk(sys_xtfile(agg_amp), agg_amp)
+#        det_xtalk.set_row(agg_amp, det_ratios)
+#
+#    #
+#    # Take difference and display
+#    #
+#    diff_xtalk = det_xtalk - sys_xtalk
+#    diff_xtalk.plot_matrix('Device crosstalk')
