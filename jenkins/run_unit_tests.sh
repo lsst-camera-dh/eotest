9e98ec8b8d0bccc28af9be19a1faefdbcf5772d7
Jim Chiang <jchiang@slac.stanford.edu>
2013-05-23 22:02:05 -0700

    set HOME so that eups has a place to write its user-dependent config stuff

--- jenkins/run_unit_tests.sh
+++ jenkins/run_unit_tests.sh
@@ -4,6 +4,8 @@ echo "Running unit tests"
 
 export SHELL=/bin/bash
 
+export HOME=`pwd`
+
 source /afs/slac/g/lsst/software/redhat5-x86_64-64bit-gcc44/DMstack/Winter2013-v6_2/loadLSST.sh
 
 setup -t v6_2 pipe_tasks
@@ -12,5 +14,7 @@ setup mysqlpython
 setup scipy
 
 export PYTHONPATH=`pwd`:${PYTHONPATH}
+
+echo `pwd`
 
 cd tests; ./run_tests.sh
