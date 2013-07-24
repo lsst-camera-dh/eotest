f364c936c4194e7fa2b2aa08a2de6aaba4aeee92
Jim Chiang <jchiang@slac.stanford.edu>
2013-07-24 12:33:12 -0700

    combine system gain and psf characterization into fe55_task

---
+++ pipeline/run_fe55_task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./pipeline_setup.sh
+
+#
+# Pipeline example
+#
+python fe55_task.py \
+    -F ${SENSOR_ID}_FE55.txt \
+    -d ${DB_CREDENTIALS} \
+    -s ${SENSOR_ID} -V e2v -o ${SENSOR_ID}/results/fe55 -v
+
+##
+## Interactive example
+##
+#python fe55_task.py \
+#    -f ${DATADIR}/sensorData/${SENSOR_ID}/fe55/debug/${SENSOR_ID}_fe55_fe55\*.fits \
+#    -s ${SENSOR_ID} -V e2v -O ${SENSOR_ID}_gains.fits -v
