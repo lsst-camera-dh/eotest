1b202d560bef17af44ef53619b997530320d84e1
Jim Chiang <jchiang@slac.stanford.edu>
2013-11-05 12:01:31 -0800

    env var for running unit tests on inadequately configured lsst-build01

--- bin/jenkins/run_unit_tests.sh
+++ bin/jenkins/run_unit_tests.sh
@@ -14,6 +14,10 @@ export HOME=`pwd`
 # This is used by the scripts to find the path to the policy subdir.
 export EOTEST_DIR=${HOME}
 
+# This is used by the unit test scripts in cases where the libgfortran.so.3
+# shared library is missing, i.e., on lsst-build01
+export TEST_SCRIPTS_DIR=${HOME}
+
 source /afs/slac/g/lsst/software/redhat5-x86_64-64bit-gcc44/DMstack/Winter2013-v6_2/loadLSST.sh
 
 # The Jenkins build of test_scripts can't/shouldn't be setup by eups, so
