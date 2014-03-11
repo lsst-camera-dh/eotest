d7b0a903da0ff4ee86fb40ec36e911a8247941e1
Jim Chiang <jchiang@slac.stanford.edu>
2014-03-11 08:56:46 -0700

    save results as a binary table

--- python/lsst/eotest/sensor/traps.py
+++ python/lsst/eotest/sensor/traps.py
@@ -4,6 +4,8 @@
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 import os
+import numpy as np
+import pyfits
 import lsst.afw.detection as afwDetect
 import lsst.afw.geom as afwGeom
 import lsst.afw.image as afwImage
@@ -12,13 +14,14 @@ import lsst.eotest.image_utils as imutils
 from MaskedCCD import MaskedCCD
 import pylab_plotter as plot
 
-def traps(ccd, gains, outfile=None, cycles=100, nsig=6, amps=imutils.allAmps):
-    if outfile is None:
-        output = open(os.devnull, 'w')
-    else:
-        output = open(outfile, 'w')
-    output.write('Amp  #x-pixel  y-pixel  trap_size\n')
+def traps(ccd, gains, outfile='traps.fits', cycles=100, nsig=6, 
+          amps=imutils.allAmps):
+    """
+    Find traps from a pocket-pumped image and write the results to a
+    FITS file as a binary table.
+    """
     traps = {}
+    row = 0
     for amp in amps:
         traps[amp] = []
         image = ccd.unbiased_and_trimmed_image(amp)
@@ -41,15 +44,35 @@ def traps(ccd, gains, outfile=None, cycles=100, nsig=6, amps=imutils.allAmps):
             colmean = imutils.mean(column)
             trap_size = (peak.getPeakValue() - colmean)*gains[amp]/cycles
             traps[amp].append((peak.getIx(), peak.getIy(), trap_size))
-            output.write(' %s  ' % imutils.channelIds[amp])
-            output.write('%5i    %5i       %.1f\n' % traps[amp][-1])
-            output.flush()
-    output.close()
+    # Write the results as a FITS binary table.
+    nrows = sum([len(traps[amp]) for amp in traps])
+    output = pyfits.HDUList()
+    output.append(pyfits.PrimaryHDU())
+    colnames = ['AMPLIFIER', 'XPOS', 'YPOS', 'TRAP_SIZE']
+    formats = 'IIII'
+    units = ['None', 'pixel', 'pixel', 'electrons']
+    columns = [np.zeros(nrows, dtype=int)]*4
+    hdu = pyfits.new_table([pyfits.Column(name=colname, format=format,
+                                          unit=unit, array=column) 
+                            for colname, format, unit, column in
+                            zip(colnames, formats, units, columns)])
+    hdu.name = 'TRAPS'
+    output.append(hdu)
+    row = 0
+    for amp in amps:
+        for xpos, ypos, trap_size in traps[amp]:
+            output['TRAPS'].data[row]['AMPLIFIER'] = amp
+            output['TRAPS'].data[row]['XPOS'] = xpos
+            output['TRAPS'].data[row]['YPOS'] = ypos
+            output['TRAPS'].data[row]['TRAP_SIZE'] = trap_size
+            row += 1
+    output.writeto(outfile, clobber=True)
+
     return traps
 
 if __name__ == '__main__':
     gains = dict([(amp, 5) for amp in imutils.allAmps])
-    outfile = 'trap_list.txt'
+    outfile = 'trap_list.fits'
     
     infile = 'work/sensorData/000-00/trap/debug/000-00_trap_ppump_debug.fits'
     mask_files = ()
