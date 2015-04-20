a2aa3de4b51167725c417113d1f11c060be6b5d7
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-20 11:16:42 -0700

    enable import of sconsUtils generated versioning info and provide a function to retrieve it even if the package is built from the tarball

--- python/lsst/eotest/__init__.py
+++ python/lsst/eotest/__init__.py
@@ -1,2 +1,11 @@
-#import lsst.utils
-#from .version import *
+import os
+import lsst.utils
+from .version import *
+
+def getVersion():
+    if __version__ != "unknown":
+        return __version__
+    import lsst.eotest
+    for item in lsst.eotest.__file__.split(os.path.sep):
+        if item.startswith('eotest-'):
+            return item[len('eotest-'):]
