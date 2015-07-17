2cdc970d1fa505853634acdec9aafe7e750e43c8
Jim Chiang <jchiang@slac.stanford.edu>
2015-07-17 16:52:12 -0700

    run git describe --tags at runtime instead of relying on build time execution by sconsUtils

--- python/lsst/eotest/__init__.py
+++ python/lsst/eotest/__init__.py
@@ -1,11 +1,19 @@
 import os
+import subprocess
 import lsst.utils
-from .version import *
 
 def getVersion():
-    if __version__ != "unknown":
-        return __version__
     import lsst.eotest
-    for item in lsst.eotest.__file__.split(os.path.sep):
-        if item.startswith('eotest-'):
-            return item[len('eotest-'):]
+    try:
+        package_dir = os.path.split(lsst.eotest.__file__)[0]
+        command = 'cd %s; git describe --tags' % package_dir
+        tag = subprocess.check_output(command, shell=True,
+                                      stderr=subprocess.STDOUT)
+        return tag.strip()
+    except subprocess.CalledProcessError:
+        # Not in a git repository, so assume this is an installation
+        # from a versioned tarball.
+        for item in lsst.eotest.__file__.split(os.path.sep):
+            if item.startswith('eotest-'):
+                return item[len('eotest-'):]
+    return 'unknown'
