bb388cbd49b5b928cee3d314403ed1ebbfc08818
Jim Chiang <jchiang@slac.stanford.edu>
2015-07-17 09:41:29 -0700

    account for system crosstalk if provided

--- python/lsst/eotest/sensor/crosstalkTask.py
+++ python/lsst/eotest/sensor/crosstalkTask.py
@@ -5,7 +5,7 @@
 """
 import os
 import lsst.eotest.image_utils as imutils
-from lsst.eotest.sensor.crosstalk import make_crosstalk_matrix
+from lsst.eotest.sensor.crosstalk import make_crosstalk_matrix, CrosstalkMatrix
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 
@@ -24,7 +24,8 @@ class CrosstalkTask(pipeBase.Task):
     _DefaultName = "CrosstalkTask"
 
     @pipeBase.timeMethod
-    def run(self, sensor_id, xtalk_files, mask_files):
+    def run(self, sensor_id, xtalk_files, mask_files,
+            system_xtalk_file=None):
         imutils.check_temperatures(xtalk_files, self.config.temp_set_point_tol,
                                    setpoint=self.config.temp_set_point,
                                    warn_only=True)
@@ -34,5 +35,8 @@ class CrosstalkTask(pipeBase.Task):
         if len(xtalk_files) == 1:
             xtalk_files = xtalk_files[0]
         xtalk = make_crosstalk_matrix(xtalk_files, mask_files=mask_files)
+        if system_xtalk_file is not None:
+            system_xtalk_matrix = CrosstalkMatrix(system_xtalk_file)
+        xtalk = xtalk - system_xtalk_matrix
         xtalk.write_fits(os.path.join(self.config.output_dir,
                                       '%s_xtalk_matrix.fits' % sensor_id))
