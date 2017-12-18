e8abe9c389530e934f98678026b985f6c386134f
Jim Chiang <jchiang@slac.stanford.edu>
2017-12-17 23:46:02 -0800

    UID:GID fix to match user vagrant ids in container

--- travis_test.sh
+++ travis_test.sh
@@ -3,9 +3,8 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 pip install nose
 pip install coveralls
-eups declare eotest -r /home/eotest -t current
+eups declare eotest -r /home/vagrant/eotest -t current
 setup eotest
 scons opt=3
-cd /home/eotest
+cd /home/vagrant/eotest
 nosetests -s --with-coverage --cover-package=lsst.eotest.image_utils,lsst.eotest.sensor
-coveralls
