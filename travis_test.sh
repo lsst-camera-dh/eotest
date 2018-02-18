715efd8e38a74c7b6260dd925fcad03f816dbc7c
Jim Chiang <jchiang@slac.stanford.edu>
2018-02-17 22:39:34 -0800

    use TRAVIS_BUILD_DIR so that coverage stats are found

--- travis_test.sh
+++ travis_test.sh
@@ -3,8 +3,8 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 pip install nose
 pip install coveralls
-eups declare eotest -r /home/vagrant/eotest -t current
+eups declare eotest -r ${TRAVIS_BUILD_DIR} -t current
 setup eotest
+cd ${TRAVIS_BUILD_DIR}
 scons opt=3
-cd /home/vagrant/eotest
 nosetests -s --with-coverage --cover-package=lsst.eotest.image_utils,lsst.eotest.sensor
