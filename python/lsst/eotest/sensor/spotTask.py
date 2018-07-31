8393832f279d9efba94f8838d59ecc7ce332cbbe
Emily L Phillips Longley <elp25@rhel6-64k.slac.stanford.edu>
2018-07-31 11:45:01 -0700

    Added moments calculation.

--- python/lsst/eotest/sensor/spotTask.py
+++ python/lsst/eotest/sensor/spotTask.py
@@ -24,28 +24,28 @@ class SpotConfig(pexConfig.Config):
     output_file = pexConfig.Field("Output filename", str, default=None)
     eotest_results_file = pexConfig.Field("EO test results filename",
                                           str, default=None)
-    verbose = pexConfig.Field("Turn verbosity on", bool, default=True)        
-        
+    verbose = pexConfig.Field("Turn verbosity on", bool, default=True)
+
 class SpotTask(pipeBase.Task):
     """Task to estimate spot moments from spot projector data."""
-    
+
     ConfigClass = SpotConfig
     _DefaultName = "SpotTask"
-    
+
     @pipeBase.timeMethod
     def run(self, sensor_id, infiles, mask_files, bias_frame=None,
-            spot_catalog=None, chiprob_min=0.1, accuracy_req=0, 
+            spot_catalog=None, chiprob_min=0.1, accuracy_req=0,
             oscan_fit_order=1):
-        
+
         imutils.check_temperatures(infiles, self.config.temp_set_point_tol,
                                    setpoint=self.config.temp_set_point,
                                    warn_only=True)
-        
+
         if self.config.verbose and spot_catalog is None:
             self.log.info("Input files:")
             for item in infiles:
                 self.log.info("  %s" % item)
-                
+
         #
         # Detect and fit spots, accumulating the results by amplifier.
         #
@@ -60,8 +60,8 @@ class SpotTask(pipeBase.Task):
                 for amp in ccd:
                     if self.config.verbose:
                         self.log.info("  amp %i" % amp)
-                    fitter.process_image(ccd, amp, oscan_fit_order=oscan_fit_order) 
-                    
+                    fitter.process_image(ccd, amp, oscan_fit_order=oscan_fit_order)
+
             if self.config.output_file is None:
                 spot_results = os.path.join(self.config.output_dir,
                                            '%s_spot_results_nsig%i.fits'
@@ -71,7 +71,7 @@ class SpotTask(pipeBase.Task):
             if self.config.verbose:
                 self.log.info("Writing spot results file to %s" % spot_results)
             fitter.write_results(outfile=spot_results)
-            namps = len(ccd)           
+            namps = len(ccd)
         else:
             fitter.read_spot_catalog(spot_catalog)
             namps = fits.open(spot_catalog)[0].header['NAMPS']
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     import glob
 
-    output_dir = '/nfs/slac/g/ki/ki19/lsst/snyder18/LSST/'
+    output_dir = '/u/ec/elp25/private/spots_testing/' #'/nfs/slac/g/ki/ki19/lsst/snyder18/LSST/'
     sensor_id = 'S11'
     mask_files = tuple()
     image_files = glob.glob('/nfs/slac/g/ki/ki19/lsst/snyder18/LSST/Data/TS8_SLAC/RTM-010/6011D_spots/*_032_*.fits')
--- python/lsst/eotest/sensor/spot_psf.py
+++ python/lsst/eotest/sensor/spot_psf.py
@@ -8,19 +8,24 @@ import lsst.afw.detection as afwDetect
 import lsst.afw.image as afwImage
 import lsst.afw.math as afwMath
 
+import scipy.optimize
+from scipy.special import erf, gammaincc
+
 import lsst.eotest.image_utils as imutils
 from MaskedCCD import MaskedCCD, MaskedCCDBiasImageException
-from fe55_psf import prect_values
+from fe55_psf import prect_values, chisq, psf_func, residuals, pixel_integral
 
+
+_sqrt2 = np.sqrt(2)
 
 class SpotMomentFit(object):
 
     def __init__(self, nsig=3, min_npix=10, max_npix=1000, outfile=None):
-        
+
         self.nsig = nsig
         self.min_npix = min_npix
-        self.max_npix = max_npix    
-    
+        self.max_npix = max_npix
+
         self.amp_set = set()
         self.outfile = outfile
         if outfile is None:
@@ -29,7 +34,7 @@ class SpotMomentFit(object):
         else:
             # Append new data to existing file.
             self.output = fits.open(self.outfile)
-    
+
     def _bg_image(self, ccd, amp, nx, ny):
         "Compute background image based on clipped local mean."
         bg_ctrl = afwMath.BackgroundControl(nx, ny, ccd.stat_ctrl)
@@ -38,7 +43,7 @@ class SpotMomentFit(object):
 
     def process_image(self, ccd, amp, sigma0=0.36, dn0=1590./5.,
                       bg_reg=(10, 10), logger=None, oscan_fit_order=1):
-        
+
         try:
             image = ccd.bias_subtracted_image(amp, fit_order=oscan_fit_order)
         except MaskedCCDBiasImageException:
@@ -58,37 +63,71 @@ class SpotMomentFit(object):
             logger.info("SpotMomentFit.process_image: threshold= %s"
                         % threshold.getValue())
         fp_set = afwDetect.FootprintSet(image, threshold)
-        
+
         ## Iterate over each footprint and calculate moments
         x0, y0 = [], [] # These are arrays to hold per-spot measurements
         prect_data = [] # This holds flattened spot footprint
+        sigmax, sigmay, dn, dn_fp, chiprob = [], [], [], [], [] # Arrays to hold the moments fitting measurements
+        chi2s, dofs = [], []
+        maxDNs = []
         failed_curve_fits = 0
         num_fp = 0
         for fp in fp_set.getFootprints():
             if fp.getNpix() < self.min_npix or fp.getNpix() > self.max_npix:
                 continue
-                
+
             num_fp +=1
             peak = [pk for pk in fp.getPeaks()][0]
-            
-            try:                
-                pars = fp.getCentroid() # placeholder for moment fitting, for now.
+            spans = fp.getSpans()
+            positions = []
+            zvals = []
+            dn_sum = 0
+
+            for span in spans:
+                y = span.getY()
+                for x in range(span.getX0(), span.getX1() + 1):
+                    positions.append((x, y))
+                    zvals.append(imarr[y][x])
+                    dn_sum += imarr[y][x]
+
+            try:
+                # Use clipped stdev as DN error estimate for all pixels
+                dn_errors = stdev*np.ones(len(positions))
+                p0 = (peak.getIx(), peak.getIy(), sigma0, sigma0, dn0)
+                pars, _ = scipy.optimize.leastsq(residuals, p0,
+                                                     args=(positions, zvals,
+                                                           dn_errors))
+                sigmax.append(pars[2])
+                sigmay.append(pars[3])
+                dn.append(pars[4])
                 x0.append(pars[0])
                 y0.append(pars[1])
+
+                dn_fp.append(dn_sum)
+                chi2 = chisq(positions, zvals, x0[-1], y0[-1],
+                             sigmax[-1], sigmay[-1], dn[-1], dn_errors)
+                dof = fp.getNpix() - 5 # number of pars for our fit
+                chiprob.append(gammaincc(dof/2., chi2/2.))
+                chi2s.append(chi2)
+                dofs.append(dof)
+                maxDNs.append(max(zvals))
+
                 prect_data_row = prect_values(peak,imarr, ixm=10, ixp=10, iym=10, iyp=10)
                 prect_data.append(prect_data_row)
             except RuntimeError:
                 failed_curve_fits +=1
-                
-        self._save_ext_data(amp, x0, y0, np.array(prect_data))
+
+        self._save_ext_data(amp, x0, y0,prect_data, sigmax, sigmay, dn, dn_fp, chiprob,
+                            chi2s, dofs, maxDNs)
         self.amp_set.add(amp)
-                 
+
         print(num_fp)
-        
+
     def numGoodFits(self, chiprob_min=0.1):
         raise NotImplementedError
-    
-    def _save_ext_data(self, amp, x0, y0, prect_data):
+
+    def _save_ext_data(self, amp, x0, y0, prect_data,sigmax, sigmay, dn, dn_fp, chiprob,
+                        chi2s, dofs, maxDNs):
         """
         Write results from the source detection and Gaussian fitting
         to the FITS extension corresponding to the specified
@@ -109,17 +148,32 @@ class SpotMomentFit(object):
                 table_hdu.data[row]['XPOS'] = x0[i]
                 table_hdu.data[row]['YPOS'] = y0[i]
                 table_hdu.data[row]['PRECT_DATA'] = prect_data[i]
+                table_hdu.data[row]['SIGMAX'] = sigmax[i]
+                table_hdu.data[row]['SIGMAY'] = sigmay[i]
+                table_hdu.data[row]['DN'] = dn[i]
+                table_hdu.data[row]['DN_FP_SUM'] = dn_fp[i]
+                table_hdu.data[row]['CHIPROB'] = chiprob[i]
+                table_hdu.data[row]['CHI2'] = chi2s[i]
+                table_hdu.data[row]['DOF'] = dofs[i]
+                table_hdu.data[row]['MAXDN'] = maxDNs[i]
             table_hdu.name = extname
             self.output[extname] = table_hdu
         except KeyError:
             #
             # Extension for this segment does not yet exist, so add it.
             #
-            colnames = ['AMPLIFIER', 'XPOS', 'YPOS', 'PRECT_DATA']
-            columns = [np.ones(len(x0))*amp, np.array(x0), np.array(y0),
-                       np.array(prect_data)]
-            formats = ['I'] + ['E']*(2) + ['441E']
-            units = ['None', 'pixel', 'pixel', 'ADU']
+            colnames = ['AMPLIFIER', 'XPOS', 'YPOS', 'PRECT_DATA','SIGMAX', 'SIGMAY', 'DN',
+                        'DN_FP_SUM', 'CHIPROB', 'CHI2', 'DOF', 'MAXDN']
+
+            columns = [np.ones(len(x0))*amp, np.array(x0), np.array(y0),np.array(prect_data),
+                       np.array(sigmax), np.array(sigmay),
+                       np.array(dn), np.array(dn_fp),
+                       np.array(chiprob), np.array(chi2s), np.array(dofs),
+                       np.array(maxDNs)]
+            formats = formats = ['I'] + ['E']*(2) + ['441E'] +['E']*(8)
+            units = ['None', 'pixel', 'pixel', 'pixel', 'pixel',
+                     'ADU', 'ADU', 'None', 'None', 'None', 'ADU',
+                     'pixel', 'pixel', 'ADU']
             fits_cols = lambda coldata: [fits.Column(name=colname,
                                                      format=format,
                                                      unit=unit,
@@ -137,7 +191,7 @@ class SpotMomentFit(object):
 
     def results(self, min_prob=0.1, amp=None):
         raise NotImplementedError
-            
+
     def write_results(self, outfile='spot_moment_params.fits'):
         self.output[0].header['NAMPS'] = len(self.amp_set)
-        fitsWriteto(self.output, outfile, overwrite=True, checksum=True)
+        fitsWriteto(self.output, outfile)#, overwrite=True, checksum=True)
