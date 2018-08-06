a12c5126000864bc836acc48df91c302c517e63e
Bela Abolfathi <babolfat@uci.edu>
2018-08-06 11:44:28 -0700

    Added new  unit tests

--- tests/image_utils_tests.py
+++ tests/image_utils_tests.py
@@ -23,11 +23,12 @@ class BiasHandlingTestCase(unittest.TestCase):
     bias_intercept = 0.5
     exptime = 1
     gain = 1
+    kwargs = {'fit_order' : 1, 'statistic' : np.mean, 'imaging' : AmplifierGeometry().imaging}
     image_file = 'test_image.fits'
+    mean_image_file = 'test_mean_image.fits'
     @classmethod
     def setUpClass(cls):
         cls.amp_geom = AmplifierGeometry()
-        cls.kwargs = {'fit_order' : 1, 'statistic' : np.mean, 'imaging' : cls.amp_geom.imaging}
         cls.bias_image = afwImage.ImageF(cls.amp_geom.full_segment)
         imarr = cls.bias_image.getArray()
         ny, nx = imarr.shape
@@ -40,32 +41,53 @@ class BiasHandlingTestCase(unittest.TestCase):
         for amp in ccd.segments:
             ccd.segments[amp].image += cls.bias_image
         ccd.writeto(cls.image_file)
+
+        cls.mean_bias_image = afwImage.ImageF(cls.amp_geom.full_segment)
+        imarr = cls.mean_bias_image.getArray()
+        ny, nx = imarr.shape
+        mu, sig = 27316.92, 6.53
+        for x in range(nx):
+            imarr[:,x] += np.random.normal(mu, sig, ny)
+        ccd = sim_tools.CCD(exptime=cls.exptime, gain=cls.gain,
+                    geometry=cls.amp_geom)
+        for amp in ccd.segments:
+            ccd.segments[amp].image += cls.mean_bias_image
+        ccd.writeto(cls.mean_image_file)
+
     @classmethod
     def tearDownClass(cls):
         os.remove(cls.image_file)
     def test_bias_func(self):
         bias_func = BiasFunc(self.bias_slope, self.bias_intercept)
         ccd = MaskedCCD(self.image_file)
-        kwargs = {'fit_order' : 1, 'statistic' : np.mean}
         for amp in ccd:
             bf_i = imutils.bias_func(ccd[amp].getImage(),
-                                     self.amp_geom.serial_overscan, **kwargs)
-            bf_m = imutils.bias_func(ccd[amp], self.amp_geom.serial_overscan, **kwargs)
+                                     self.amp_geom.serial_overscan, **self.kwargs)
+            bf_m = imutils.bias_func(ccd[amp], self.amp_geom.serial_overscan, **self.kwargs)
             for y in range(2022):
                 self.assertEqual(bf_i(y), bf_m(y))
                 self.assertAlmostEqual(bias_func(y), bf_m(y))
     def test_bias_image(self):
         ccd = MaskedCCD(self.image_file)
         overscan = makeAmplifierGeometry(self.image_file)
-        kwargs = {'fit_order' : 1, 'statistic' : np.mean}
+        ccd_mean = MaskedCCD(self.mean_image_file)
+        overscan_mean = makeAmplifierGeometry(self.mean_image_file)
         for amp in ccd:
             for method in ['mean', 'row', 'func']:
-                my_bias_image = imutils.bias_image(ccd[amp], 
+                if method == 'mean':
+		    my_bias_image = imutils.bias_image(ccd_mean[amp],
+                                                    overscan_mean.serial_overscan,
+                                                    method, **self.kwargs)
+                    fracdiff = ((self.mean_bias_image.getArray() - my_bias_image.getArray())
+                        /self.mean_bias_image.getArray())
+                    self.assertTrue(max(np.abs(fracdiff.flat)) < 1.5e-3)
+                else:
+		    my_bias_image = imutils.bias_image(ccd[amp],
                                                     overscan.serial_overscan,
-                                                    method, **kwargs)
-                fracdiff = ((self.bias_image.getArray() - my_bias_image.getArray())
-                        /self.bias_image.getArray())
-                self.assertTrue(max(np.abs(fracdiff.flat)) < 1e-6)
+                                                    method, **self.kwargs)
+                    fracdiff = ((self.bias_image.getArray() - my_bias_image.getArray())
+                            /self.bias_image.getArray())
+                    self.assertTrue(max(np.abs(fracdiff.flat)) < 1e-6)
     def test_unbias_and_trim(self):
         ccd = MaskedCCD(self.image_file)
         overscan = makeAmplifierGeometry(self.image_file)
@@ -74,13 +96,16 @@ class BiasHandlingTestCase(unittest.TestCase):
                 image = imutils.unbias_and_trim(ccd[amp], 
                                                  overscan.serial_overscan, method, **self.kwargs)
                 imarr = image.getImage().getArray()
-                self.assertTrue(max(np.abs(imarr.flat)) < 1e-6)
-                #
-                # Test of corresponding MaskedCCD method.
-                #
-                image = ccd.unbiased_and_trimmed_image(amp)
-                imarr = image.getImage().getArray()
-                self.assertTrue(max(np.abs(imarr.flat)) < 1e-6)
+                if method == 'mean':
+		    self.assertTrue(max(np.abs(imarr.flat)) < 2)
+		else:
+		    self.assertTrue(max(np.abs(imarr.flat)) < 1e-6)
+	    	    #
+            	    # Test of corresponding MaskedCCD method.
+            	    #
+            	    image = ccd.unbiased_and_trimmed_image(amp, overscan.serial_overscan, fit_order=1)
+            	    imarr = image.getImage().getArray()
+	    	    self.assertTrue(max(np.abs(imarr.flat)) < 1e-6)
     def test_bias_row(self):
         ccd = MaskedCCD(self.image_file)
         overscan = makeAmplifierGeometry(self.image_file)
@@ -89,10 +114,9 @@ class BiasHandlingTestCase(unittest.TestCase):
             br_i = imutils.bias_row(ccd[amp].getImage(), overscan.serial_overscan)
             # Masked image
             br_m = imutils.bias_row(ccd[amp], overscan.serial_overscan)
-            ni, nm = len(br_i), len(br_m)
-            self.assertEqual(ni, nm)
-            for y in range(nm):
-            	self.assertEqual(br_i[y], br_m[y])
+	    ny = len(br_i)
+	    for ii in range(ny):
+	        self.assertEqual(br_i[ii], br_m[ii])
     def test_stack(self):
         ccd = MaskedCCD(self.image_file)
         overscan = makeAmplifierGeometry(self.image_file)
@@ -100,17 +124,13 @@ class BiasHandlingTestCase(unittest.TestCase):
         for method in ['mean', 'row', 'func']:
             corrected = []
             for image in ccd.values():
-                corrected.append(imutils.unbias_and_trim(image, overscan.serial_overscan, 
-                                method, **self.kwargs).getImage())
+                corrected.append(imutils.unbias_and_trim(image, overscan.serial_overscan, method, **self.kwargs).getImage())
             stacked = imutils.stack(corrected)
-            stats.append(np.mean(stacked.getArray()))
-        self.assertAlmostEqual(stats[0], stats[1])
-        self.assertAlmostEqual(stats[1], stats[2])
-        
-
-
-
-
+	    imarr = stacked.getArray()
+	    if method == 'mean':
+		self.assertTrue(max(np.abs(imarr.flat)) < 2)
+	    else:
+	    	self.assertTrue(max(np.abs(imarr.flat)) < 1e-6)
 
 
 
