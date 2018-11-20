e36f65767fd27796360bdfc47229f1ef10f92e1f
[Emily PhillipsLongley] <[elp25@duke.edu]>
2018-11-20 11:14:39 -0500

    Added unit test and error in BFTask.py.

--- python/lsst/eotest/sensor/BFTask.py
+++ python/lsst/eotest/sensor/BFTask.py
@@ -1,8 +1,6 @@
 """
-@brief Compute the brighter fatter correlation coefficients.  Input is
-flat pairs.  Output is the nearest neighbor coefficients per amp.
-
-@author E. Phillips Longley <elp25@duke.edu>
+brief Compute the brighter fatter correlation coefficients.  Input is
+flat pairs.  Based on the LSST DM cdoe.  Output is the nearest neighbor coefficients per amp.
 """
 import os
 import glob
@@ -55,12 +53,12 @@ def find_flats(flats):
 class BFConfig(pexConfig.Config):
     """Configuration for BFTask"""
     maxLag = pexConfig.Field("Maximum lag",
-                             int, default=8)
+                             int, default=1)
     nPixBorder = pexConfig.Field("Number of pixels to clip on the border",
                                 int, default=10)
     nSigmaClip = pexConfig.Field("Number of sigma to clip for corr calc", int,
     default = 3)
-    output_dir = pexConfig.Field("Output directory", str, default="/u/ec/elp25/private/TESTBFOUTPUT/")
+    output_dir = pexConfig.Field("Output directory", str, default=".")
     eotest_results_file = pexConfig.Field("EO test results filename",
                                           str, default=None)
     verbose = pexConfig.Field("Turn verbosity on", bool, default=True)
@@ -68,9 +66,9 @@ class BFConfig(pexConfig.Config):
 
 
 class BFResults(object):
-    def __init__(self, C10s, C01s, means):
-        self.C10s = C10s
-        self.C01s = C01s
+    def __init__(self, xcorrs, ycorrs, means):
+        self.xcorrs = xcorrs
+        self.ycorrs = ycorrs
         self.means = means
 
 class BFTask(pipeBase.Task):
@@ -81,9 +79,11 @@ class BFTask(pipeBase.Task):
     @pipeBase.timeMethod
 
     def run(self, sensor_id, flat_files,meanidx=0,single_pairs=True, bias_frames=None,dark_frame = None, mask_files = ()):
-        """Compute the average nearest neighbor correlation coefficients for
+        """
+        Compute the average nearest neighbor correlation coefficients for
         all flat pairs given for a particular exposure time.  Additionally
-        store the flat means per amp."""
+        store the flat means per amp.
+        """
         if single_pairs:
             flats = find_flats(flat_files)
         else:
@@ -94,24 +94,28 @@ class BFTask(pipeBase.Task):
         # List with some number of flat pairs per exposure
         # [[(flat1,flat2),(flat1,flat2)],[(flat1,flat2),(flat1,flat2)]]
 
-        C10s = {}
-        C01s = {}
+        xcorrs = {}
+        ycorrs = {}
         means = {}
         BFResults = {}
 
         for amp in all_amps:
             print('on amp',amp)
-            C10_exp = []
-            C01_exp = []
+            xcorr_exp = []
+            xcorr_exp_err = []
+            ycorr_exp = []
+            ycorr_exp_err = []
             mean_exp = []
 
             for exposure in range(len(flats)):
-                C10_amp = []
-                C01_amp = []
+                xcorr_amp = []
+                xcorr_amp_err = []
+                ycorr_amp = []
+                ycorr_amp_err = []
                 mean_amp = []
                 if dark_frame:
                     ccd_dark = MaskedCCD(dark_frame,mask_files = mask_files)
-                    dark_frame = ccd_dark.unbiased_and_trimmed_image(amp)
+                    dark_image = ccd_dark.unbiased_and_trimmed_image(amp)
                 for flat_pair in flats[exposure]:
                     print(flat_pair)
                     ccd1 = MaskedCCD(flat_pair[0],mask_files = mask_files)
@@ -119,48 +123,55 @@ class BFTask(pipeBase.Task):
 
                     image1 = ccd1.unbiased_and_trimmed_image(amp)
                     image2 = ccd2.unbiased_and_trimmed_image(amp)
-                    prepped_image1, mean1 = self.prep_image(image1,dark_frame)
-                    prepped_image2, mean2 = self.prep_image(image2,dark_frame)
+                    prepped_image1, mean1 = self.prep_image(image1,dark_image)
+                    prepped_image2, mean2 = self.prep_image(image2,dark_image)
                     #Calculate the average mean of the pair
                     avemean = (mean1+mean2)/2
+                    print(avemean)
 
                     #Compute the correlations
-                    corr = self.crossCorrelate(prepped_image1,prepped_image2)
-                    C10_amp.append(corr[1][0]/corr[0][0])
-                    C01_amp.append(corr[0][1]/corr[0][0])
+                    corr, corr_err = crossCorrelate(prepped_image1,prepped_image2,self.config.maxLag, self.config.nPixBorder,self.config.nSigmaClip,self.config.backgroundBinSize)
+                    xcorr_amp.append(corr[1][0]/corr[0][0])
+                    xcorr_amp_err.append(corr_err[1][0])
+                    ycorr_amp_err.append(corr_err[0][1])
+                    ycorr_amp.append(corr[0][1]/corr[0][0])
                     mean_amp.append(avemean)
 
-                C10_exp.append(np.mean(C10_amp))
-                C01_exp.append(np.mean(C01_amp))
+                xcorr_exp.append(np.mean(xcorr_amp))
+                xcorr_exp_err.append(np.mean(xcorr_amp_err))
+                ycorr_exp.append(np.mean(ycorr_amp))
+                ycorr_exp_err.append(np.mean(ycorr_amp_err))
                 mean_exp.append(np.mean(mean_amp))
 
             #Store cov/var
-            C10s[amp] = C10_exp
-            C01s[amp] = C01_exp
+            xcorrs[amp] = xcorr_exp
+            ycorrs[amp] = ycorr_exp
             means[amp] = mean_exp
-            BFResults[amp] = [C10_exp,C01_exp,mean_exp]
+            BFResults[amp] = [xcorr_exp,xcorr_exp_err,ycorr_exp,ycorr_exp_err,mean_exp]
 
         self.writeEotestOutput(BFResults, all_amps,sensor_id,meanidx)
 
-        return
+        return BFResults
 
 
     def writeEotestOutput(self,BFResults,all_amps,sensor_id,meanidx=0):
 
         outfile = os.path.join(self.config.output_dir,
-                               '%s_bftest.fits' % sensor_id)
+                               '%s_bf.fits' % sensor_id)
         output = fits.HDUList()
         output.append(fits.PrimaryHDU())
         colnames = []
         units = []
         columns = []
         for amp in all_amps:
-            colnames.extend(['AMP%02i_C10' % amp, 'AMP%02i_C01' % amp,
-            'AMP%02i_MEAN' % amp])
-            units.extend(['Unitless', 'Unitless','Unitless'])
+            colnames.extend(['AMP%02i_xcorr' % amp, 'AMP%02i_xcorr_err' % amp, 'AMP%02i_ycorr' % amp,
+            'AMP%02i_ycorr_err' % amp,'AMP%02i_MEAN' % amp])
+            units.extend(['Unitless', 'Unitless','Unitless','Unitless','ADU'])
             columns.extend([np.array(BFResults[amp][0], dtype=np.float),
                             np.array(BFResults[amp][1], dtype=np.float),
-                            np.array(BFResults[amp][2], dtype=np.float)])
+                            np.array(BFResults[amp][2], dtype=np.float),
+                            np.array(BFResults[amp][3], dtype=np.float),
+                            np.array(BFResults[amp][4], dtype=np.float)])
         formats = 'E'*len(colnames)
         fits_cols = [fits.Column(name=colnames[i], format=formats[i],
                                  unit=units[i], array=columns[i])
@@ -179,16 +190,20 @@ class BFTask(pipeBase.Task):
         results = EOTestResults(results_file, namps=len(all_amps))
 
         for amp in all_amps:
-            results.add_seg_result(amp, 'C10', BFResults[amp][0][meanidx])
-            results.add_seg_result(amp, 'C01', BFResults[amp][1][meanidx])
-            results.add_seg_result(amp, 'MEAN', BFResults[amp][2][meanidx])
+            results.add_seg_result(amp, 'xcorr', BFResults[amp][0][meanidx])
+            results.add_seg_result(amp, 'xcorr_err', BFResults[amp][1][meanidx])
+            results.add_seg_result(amp, 'ycorr', BFResults[amp][2][meanidx])
+            results.add_seg_result(amp, 'ycorr_err', BFResults[amp][3][meanidx])
+            results.add_seg_result(amp, 'MEAN', BFResults[amp][4][meanidx])
 
 
         results.write(clobber=True)
 
     def prep_image(self,exp,dark_image):
-        """Crop the image to avoid edge effects based on the Config border
-        parameter. Additionally, if there is a dark image, subtract."""
+        """
+        Crop the image to avoid edge effects based on the Config border
+        parameter. Additionally, if there is a dark image, subtract.
+        """
 
         # Clone so that we don't modify the input image.
         local_exp = exp.clone()
@@ -211,12 +226,10 @@ class BFTask(pipeBase.Task):
 
         return local_exp, mean
 
-    def crossCorrelate(self,maskedimage1, maskedimage2):
-
-        maxLag = self.config.maxLag
-        border = self.config.nPixBorder
-        sigma = self.config.nSigmaClip
-
+def crossCorrelate(maskedimage1, maskedimage2, maxLag, border,sigma,binsize):
+        """
+        Calculate the correlation coefficients.
+        """
         sctrl = afwMath.StatisticsControl()
         sctrl.setNumSigmaClip(sigma)
 
@@ -224,8 +237,7 @@ class BFTask(pipeBase.Task):
         diff = maskedimage1.clone()
         diff -= maskedimage2.getImage()
 
-        # Subtract background.  It should be a constant, but it isn't always
-        binsize = self.config.backgroundBinSize
+        # Subtract background. 
         nx = diff.getWidth()//binsize
         ny = diff.getHeight()//binsize
         bctrl = afwMath.BackgroundControl(nx, ny, sctrl, afwMath.MEANCLIP)
@@ -240,6 +252,7 @@ class BFTask(pipeBase.Task):
         dim0 -= afwMath.makeStatistics(dim0, afwMath.MEANCLIP, sctrl).getValue()
         width, height = dim0.getDimensions()
         xcorr = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
+        xcorr_err = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
 
         for xlag in range(maxLag + 1):
             for ylag in range(maxLag + 1):
@@ -247,5 +260,12 @@ class BFTask(pipeBase.Task):
                 dim_xy -= afwMath.makeStatistics(dim_xy, afwMath.MEANCLIP, sctrl).getValue()
                 dim_xy *= dim0
                 xcorr[xlag, ylag] = afwMath.makeStatistics(dim_xy,afwMath.MEANCLIP, sctrl).getValue()
+                dim_xy_array = dim_xy.getImage().getArray().flatten()/xcorr[0][0]
+                N = len(dim_xy_array.flatten())
+                if xlag!=0 and ylag!=0:
+                        f = (1+xcorr[xlag,ylag]/xcorr[0][0])/(1-xcorr[xlag,ylag]/xcorr[0][0]) 
+                        xcorr_err[xlag, ylag] = (np.std(dim_xy_array)/np.sqrt(N))*np.sqrt(f)
+                else:
+                        xcorr_err[xlag, ylag] = 0
+        return xcorr,xcorr_err
 
-        return xcorr
---
+++ tests/BFTest.py
@@ -0,0 +1,53 @@
+"""
+@brief Unit tests for BFTask module.
+"""
+import os
+import unittest
+import numpy as np
+import lsst.eotest.image_utils as imutils
+from lsst.eotest.sensor import MaskedCCD
+import lsst.eotest.sensor.sim_tools as sim_tools
+from lsst.eotest.sensor.BFTask import crossCorrelate
+
+
+class BFTestCase(unittest.TestCase):
+    """Test case for BrightFatter code.
+        Modeled after BrightPixelsTestCase"""
+
+    def setUp(self):
+        self.flat_file1 = 'brighter_fatter_test1.fits'
+        self.flat_file2 = 'brighter_fatter_test2.fits'
+        self.emin = 10
+        self.exptime = 10
+        self.gain = 5
+        self.ccdtemp = -100
+        self.maxLag = 1
+        self.nPixBorder = 10
+        self.nSigmaClip = 3
+        self.backgroundBinSize = 128
+        self.bias_level, self.bias_sigma = 1e2, 4
+        self.ccd1 = sim_tools.CCD(exptime=self.exptime, gain=self.gain,
+                                 ccdtemp=self.ccdtemp)
+        self.ccd1.add_bias(self.bias_level, self.bias_sigma)
+
+        self.ccd1.writeto(self.flat_file1)
+        self.ccd2 = sim_tools.CCD(exptime=self.exptime, gain=self.gain,
+                                 ccdtemp=self.ccdtemp)
+        self.ccd2.add_bias(self.bias_level, self.bias_sigma)
+
+        self.ccd2.writeto(self.flat_file2)
+
+    def tearDown(self):
+        os.remove(self.flat_file1)
+        os.remove(self.flat_file2)
+
+    def test_find_pixel_defects(self):
+        ccd1 = MaskedCCD(self.flat_file1)
+        ccd2 = MaskedCCD(self.flat_file2)
+        for amp in ccd1:
+            xcorr, xcorr_err = crossCorrelate(ccd1.unbiased_and_trimmed_image(amp), ccd2.unbiased_and_trimmed_image(amp), self.maxLag, self.nPixBorder, self.nSigmaClip, self.backgroundBinSize )
+            self.assertTrue(1>xcorr[0][1] and 1>xcorr[1][0])
+
+
+if __name__ == '__main__':
+    unittest.main()
