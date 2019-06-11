25ece48c6c6533eead3e8dfe24ef161d1257639e
snyder18 <snyder18@stanford.edu>
2019-06-11 15:01:45 -0700

    Testing out multiprocessing functionality for crosstalk calibration and fitting.

--- python/lsst/eotest/raft/raft_crosstalk.py
+++ python/lsst/eotest/raft/raft_crosstalk.py
@@ -8,6 +8,26 @@ from lsst.eotest.sensor import CrosstalkTask
 import argparse
 import errno
 import multiprocessing as mp
+import time
+
+def median_stack_images(main_dir, output_dir, sensor_list, xpos, ypos):
+
+    print(xpos, ypos)
+    for sensor_id in sensor_list:
+
+        try:
+            os.makedirs(os.path.join(output_dir, sensor_id))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        xtalk_files = glob.glob(os.path.join(main_dir, 'xtalk_{0}_{1}*'.format(xpos, ypos),
+                                             '*_{0}.fits'.format(sensor_id)))
+        infile = os.path.join(output_dir, sensor_id,
+                              '{0}_{1}_{2}_median.fits'.format(sensor_id, xpos, ypos))
+        imutils.fits_median_file(xtalk_files, infile, bitpix=-32)
+
+    return True
 
 class CrosstalkManager():
 
@@ -32,22 +52,11 @@ class CrosstalkManager():
         infiles = []
         infiles2 = []
         for xpos, ypos in self.xtalk_dict[sensor_id]:
-            xtalk_files = glob.glob(os.path.join(self.main_dir, 'xtalk_{0}_{1}*'.format(xpos, ypos),
-                                                 '*_{0}.fits'.format(sensor_id)))[0]
-#            infile = os.path.join(self.output_dir, sensor_id,
-#                                  '{0}_{1}_{2}_median.fits'.format(sensor_id, xpos, ypos))
-#            imutils.fits_median_file(xtalk_files, infile, bitpix=-32)
+            xtalk_files = os.path.join(output_dir, sensor_id, '{0}_{1}_{2}_median.fits'.format(sensor_id, xpos, ypos))
             infiles.append(xtalk_files)
-#            print('made file {0}'.format(infile))
 
-            xtalk_files2 = glob.glob(os.path.join(self.main_dir, 'xtalk_{0}_{1}*'.format(xpos, ypos),
-                                                  '*_{0}.fits'.format(sensor_id2)))[0]
-#            infile2 = os.path.join(self.output_dir, sensor_id2,
-#                                   '{0}_{1}_{2}_median.fits'.format(sensor_id2, xpos, ypos))
-#            imutils.fits_median_file(xtalk_files2, infile2, bitpix=-32)
+            xtalk_files2 = os.path.join(output_dir, sensor_id2, '{0}_{1}_{2}_median.fits'.format(sensor_id2, xpos, ypos))
             infiles2.append(xtalk_files2)
-#            print('made file {0}'.format(infile2))
-
 
         print("running {0}, {1} crosstalk".format(sensor_id, sensor_id2))
         
@@ -95,6 +104,7 @@ def BOT(main_dir, output_dir='./'):
     ## Construct dictionary of acquisition information
     xtalk_dict = dict()
     directory_list = [x.path for x in os.scandir(main_dir) if os.path.isdir(x.path)]
+    pos_list = []
     for acquisition_dir in directory_list:
         basename = os.path.basename(acquisition_dir)
         if "xtalk" not in basename:
@@ -102,15 +112,29 @@ def BOT(main_dir, output_dir='./'):
         xpos, ypos = basename.split('_')[-4:-2]
         central_sensor = get_central_sensor(xpos, ypos)
         pos = (xpos, ypos)
+        pos_list.append(pos)
         if central_sensor in xtalk_dict:
             xtalk_dict[central_sensor].add(pos)
         else:
             xtalk_dict[central_sensor] = {pos}
 
-    print(mp.cpu_count())
+    ## Create median stacks for all files
+    a = time.time()
+    calib_pool = mp.Pool(processes=8)
+    calib_results = []
+    for xpos, ypos in pos_list:
+
+        calib_results.append(calib_pool.apply_async(median_stack_images, args=(main_dir, output_dir, sensor_list, xpos, ypos)))
+
+    calib_results = [p.get() for p in calib_results]
+    b = time.time()
+    print(b-a)
+        
+    ## Run crosstalk for each sensor pair
+    c = time.time()
     sensor_pairs = list(itertools.product(xtalk_dict, xtalk_dict))
-    pool = mp.Pool(processes=4)
-    results = []
+    xtalk_pool = mp.Pool(processes=8)
+    xtalk_results = []
 
     xtalk_manager = CrosstalkManager(main_dir, xtalk_dict, gains_dict, bias_dict, output_dir=output_dir)
     ## For each sensor pair, calculate crosstalk
@@ -127,9 +151,11 @@ def BOT(main_dir, output_dir='./'):
             if e.errno != errno.EEXIST:
                 raise
 
-        results.append(pool.apply_async(xtalk_manager.run_crosstalk_task, args = (sensor_id, sensor_id2)))
+        xtalk_results.append(xtalk_pool.apply_async(xtalk_manager.run_crosstalk_task, args = (sensor_id, sensor_id2)))
 
-    results = [p.get() for p in results]
+    xtalk_results = [p.get() for p in xtalk_results]
+    d = time.time()
+    print(d-c)
 
 if __name__ == '__main__':
 
