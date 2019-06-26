e45f6ba5f36be2d3672a2e366dacbabb92fbe741
James Chiang <jchiang@slac.stanford.edu>
2019-06-26 15:34:37 -0700

    instrument the code to find segfaults under JH/parsl; refactor parts that use numpy.flatten to avoid reading underlying ndarrays from lsst code into python

--- python/lsst/eotest/sensor/BFTask.py
+++ python/lsst/eotest/sensor/BFTask.py
@@ -6,6 +6,8 @@ coefficients per amp.
 import os
 import glob
 from collections import namedtuple
+import subprocess
+import psutil
 import numpy as np
 from astropy.io import fits
 import lsst.afw.geom as afwGeom
@@ -67,6 +69,20 @@ class BFTask(pipeBase.Task):
     ConfigClass = BFConfig
     _DefaultName = "BrighterFatterTask"
 
+    def log_mem_info(self, message, amp=None):
+        if self.process is None:
+            return
+        rss_mem = self.process.memory_info().rss/1024.**3
+        amp_info = ", amp %d" % amp if amp is not None else ''
+        self.log.info('BFTask, %s: rss_mem=%.2f GB; %s',
+                      self.process.pid, rss_mem, message + amp_info)
+
+    def _set_process(self):
+        if os.environ.get('LCATR_LOG_MEM_INFO', False) == 'True':
+            self.process = psutil.Process(os.getpid())
+        else:
+            self.process = None
+
     @pipeBase.timeMethod
     def run(self, sensor_id, flat_files, single_pairs=True,
             dark_frame=None, mask_files=(), flat2_finder=None,
@@ -76,16 +92,24 @@ class BFTask(pipeBase.Task):
         all flat pairs given for a particular exposure time.  Additionally
         store the flat means per amp.
         """
+        self._set_process()
+        self.log_mem_info('entered run method')
+        command = '(ulimit -a) >& ulimit_info_{}.txt'.format(sensor_id)
+        subprocess.check_call(command, shell=True)
         if dark_frame is not None:
+            self.log_mem_info('creating ccd_dark')
             ccd_dark = MaskedCCD(dark_frame, mask_files=mask_files)
 
         if single_pairs:
             if flat2_finder is None:
                 flat2_finder = find_flat2
+            self.log_mem_info('calling find_flats')
             flats = find_flats(flat_files, flat2_finder=flat2_finder)
         else:
+            self.log_mem_info('calling split_flats')
             flats = split_flats(flat_files)
 
+        self.log_mem_info('calling imutils.allAmps')
         all_amps = imutils.allAmps(flat_files[0])
 
         # List with some number of flat pairs per exposure
@@ -95,19 +119,26 @@ class BFTask(pipeBase.Task):
 
         for flat_pair in flats:
             self.log.info("%s\n%s", *flat_pair)
+            self.log_mem_info('making ccd1')
             ccd1 = MaskedCCD(flat_pair[0], mask_files=mask_files,
                              bias_frame=bias_frame)
+            self.log_mem_info('making ccd2')
             ccd2 = MaskedCCD(flat_pair[1], mask_files=mask_files,
                              bias_frame=bias_frame)
 
             for amp in all_amps:
                 self.log.info('on amp %s', amp)
+                self.log_mem_info('making dark_image', amp=amp)
                 dark_image = None if dark_frame is None \
                              else ccd_dark.unbiased_and_trimmed_image(amp)
 
+                self.log_mem_info('making image1', amp=amp)
                 image1 = ccd1.unbiased_and_trimmed_image(amp)
+                self.log_mem_info('making image2', amp=amp)
                 image2 = ccd2.unbiased_and_trimmed_image(amp)
+                self.log_mem_info('calling prep_image(image1)', amp=amp)
                 prepped_image1, mean1 = self.prep_image(image1, dark_image)
+                self.log_mem_info('calling prep_image(image2)', amp=amp)
                 prepped_image2, mean2 = self.prep_image(image2, dark_image)
 
                 # Calculate the average mean of the pair.
@@ -115,10 +146,12 @@ class BFTask(pipeBase.Task):
                 self.log.info('%s', avemean)
 
                 # Compute the correlations.
-                corr, corr_err = crossCorrelate(prepped_image1, prepped_image2,
-                                                self.config.maxLag,
-                                                self.config.nSigmaClip,
-                                                self.config.backgroundBinSize)
+                self.log_mem_info('calling crossCorrelate', amp=amp)
+                corr, corr_err \
+                    = self.crossCorrelate(prepped_image1, prepped_image2,
+                                          self.config.maxLag,
+                                          self.config.nSigmaClip,
+                                          self.config.backgroundBinSize)
 
                 # Append the per-amp values for this pair to the
                 # corresponding lists.
@@ -128,6 +161,7 @@ class BFTask(pipeBase.Task):
                 BFResults[amp].ycorr_err.append(corr_err[0][1])
                 BFResults[amp].mean.append(avemean)
 
+        self.log_mem_info('calling write_eotest_output')
         self.write_eotest_output(BFResults, sensor_id, meanidx=meanidx)
 
         return BFResults
@@ -209,57 +243,75 @@ class BFTask(pipeBase.Task):
 
         return local_exp, mean
 
+    def crossCorrelate(self, maskedimage1, maskedimage2, maxLag, sigma,
+                       binsize):
+        """
+        Calculate the correlation coefficients.
+        """
+        process = psutil.Process(os.getpid())
+        self.log_mem_info('entered crossCorrelate')
+        sctrl = afwMath.StatisticsControl()
+        sctrl.setNumSigmaClip(sigma)
 
-def crossCorrelate(maskedimage1, maskedimage2, maxLag, sigma, binsize):
-    """
-    Calculate the correlation coefficients.
-    """
-    sctrl = afwMath.StatisticsControl()
-    sctrl.setNumSigmaClip(sigma)
+        # Diff the images.
+        self.log_mem_info('maskedimage1.clone()')
+        diff = maskedimage1.clone()
+        self.log_mem_info('diff -= maskedimage2.getImage()')
+        diff -= maskedimage2.getImage()
 
-    # Diff the images.
-    diff = maskedimage1.clone()
-    diff -= maskedimage2.getImage()
+        # Subtract background.
+        nx = diff.getWidth()//binsize
+        ny = diff.getHeight()//binsize
+        self.log_mem_info('creating bctrl')
+        bctrl = afwMath.BackgroundControl(nx, ny, sctrl, afwMath.MEANCLIP)
+        self.log_mem_info('creating bkgd')
+        bkgd = afwMath.makeBackground(diff, bctrl)
+        self.log_mem_info('creating bgImg')
+        bgImg = bkgd.getImageF(afwMath.Interpolate.CUBIC_SPLINE,
+                               afwMath.REDUCE_INTERP_ORDER)
 
-    # Subtract background.
-    nx = diff.getWidth()//binsize
-    ny = diff.getHeight()//binsize
-    bctrl = afwMath.BackgroundControl(nx, ny, sctrl, afwMath.MEANCLIP)
-    bkgd = afwMath.makeBackground(diff, bctrl)
-    bgImg = bkgd.getImageF(afwMath.Interpolate.CUBIC_SPLINE,
-                           afwMath.REDUCE_INTERP_ORDER)
+        self.log_mem_info('diff -= bgImg')
+        diff -= bgImg
 
-    diff -= bgImg
+        # Measure the correlations
+        x0, y0 = diff.getXY0()
+        width, height = diff.getDimensions()
+        bbox_extent = afwGeom.Extent2I(width - maxLag, height - maxLag)
 
-    # Measure the correlations
-    x0, y0 = diff.getXY0()
-    width, height = diff.getDimensions()
-    bbox_extent = afwGeom.Extent2I(width - maxLag, height - maxLag)
+        bbox = afwGeom.Box2I(afwGeom.Point2I(x0, y0), bbox_extent)
+        dim0 = diff[bbox].clone()
+        self.log_mem_info('dim0 -= afwMath.makeStatistics(...)')
+        dim0 -= afwMath.makeStatistics(dim0, afwMath.MEANCLIP,
+                                       sctrl).getValue()
 
-    bbox = afwGeom.Box2I(afwGeom.Point2I(x0, y0), bbox_extent)
-    dim0 = diff[bbox].clone()
-    dim0 -= afwMath.makeStatistics(dim0, afwMath.MEANCLIP, sctrl).getValue()
+        xcorr = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
+        xcorr_err = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
 
-    xcorr = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
-    xcorr_err = np.zeros((maxLag + 1, maxLag + 1), dtype=np.float64)
-
-    for xlag in range(maxLag + 1):
-        for ylag in range(maxLag + 1):
-            bbox_lag = afwGeom.Box2I(afwGeom.Point2I(x0 + xlag, y0 + ylag),
-                                     bbox_extent)
-            dim_xy = diff[bbox_lag].clone()
-            dim_xy -= afwMath.makeStatistics(dim_xy, afwMath.MEANCLIP,
-                                             sctrl).getValue()
-            dim_xy *= dim0
-            xcorr[xlag, ylag] = afwMath.makeStatistics(
-                dim_xy, afwMath.MEANCLIP, sctrl).getValue()
-            dim_xy_array = dim_xy.getImage().getArray().flatten()/xcorr[0][0]
-            N = len(dim_xy_array.flatten())
-            if xlag != 0 and ylag != 0:
-                f = (1+xcorr[xlag, ylag]/xcorr[0][0]) / \
-                    (1-xcorr[xlag, ylag]/xcorr[0][0])
-                xcorr_err[xlag, ylag] = (
-                    np.std(dim_xy_array)/np.sqrt(N))*np.sqrt(f)
-            else:
-                xcorr_err[xlag, ylag] = 0
-    return xcorr, xcorr_err
+        for xlag in range(maxLag + 1):
+            for ylag in range(maxLag + 1):
+                self.log_mem_info('xlag = %d, ylag = %d' % (xlag, ylag))
+                bbox_lag = afwGeom.Box2I(afwGeom.Point2I(x0 + xlag, y0 + ylag),
+                                         bbox_extent)
+                dim_xy = diff[bbox_lag].clone()
+                self.log_mem_info('dim_xy -= afwMath.makeStatistics(...)')
+                dim_xy -= afwMath.makeStatistics(dim_xy, afwMath.MEANCLIP,
+                                                 sctrl).getValue()
+                dim_xy *= dim0
+                self.log_mem_info('computing xcorr[xlag, ylag]')
+                xcorr[xlag, ylag] = afwMath.makeStatistics(
+                    dim_xy, afwMath.MEANCLIP, sctrl).getValue()
+                self.log_mem_info('dim_xy_image =')
+                dim_xy_image = dim_xy.getImage()
+                self.log_mem_info('dim_xy_array =')
+                dim_xy_array = dim_xy_image.array/xcorr[0][0]
+                self.log_mem_info('N = np.prod(...)')
+                N = np.prod(dim_xy_array.shape)
+                if xlag != 0 and ylag != 0:
+                    self.log_mem_info('f = (1+xcorr...)')
+                    f = (1+xcorr[xlag, ylag]/xcorr[0][0]) / \
+                        (1-xcorr[xlag, ylag]/xcorr[0][0])
+                    xcorr_err[xlag, ylag] \
+                        = np.std(dim_xy_array)/np.sqrt(N)*np.sqrt(f)
+                else:
+                    xcorr_err[xlag, ylag] = 0
+        return xcorr, xcorr_err
