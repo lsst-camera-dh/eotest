37bfc5af265102edfe9b3fbb49e79210faf0d19c
Seth Digel <digel@stanford.edu>
2019-08-27 17:44:17 -0700

    Now uses ptc_func to evaluate curve to overlay on PTC plots

--- python/lsst/eotest/sensor/EOTestPlots.py
+++ python/lsst/eotest/sensor/EOTestPlots.py
@@ -20,6 +20,7 @@ import lsst.afw.math as afwMath
 import lsst.afw.display.ds9 as ds9
 import lsst.eotest.image_utils as imutils
 from lsst.eotest.Estimator import Estimator
+from lsst.eotest.sensor.ptcTask import ptc_func
 from . import pylab_plotter as plot
 from .MaskedCCD import MaskedCCD
 from .EOTestResults import EOTestResults
@@ -442,7 +443,8 @@ class EOTestPlots(object):
                 ptc_gain_error = self.results['PTC_GAIN_ERROR'][amp-1]
                 ptc_a00 = self.results['PTC_A00'][amp-1]
                 ptc_a00_error = self.results['PTC_A00_ERROR'][amp-1]
-                plot.curve(xx, xx/ptc_gain, oplot=1, color='b', lineStyle=':')
+                plot.curve(xx, ptc_func((ptc_a00, ptc_gain, ptc_noise), xx),
+                           oplot=1, color='b', lineStyle=':')
                 note = 'Amp %i\nGain = %.2f +/- %.2f\nA00 = %.1e +/- %.1e'\
                     % (amp, ptc_gain, ptc_gain_error, ptc_a00, ptc_a00_error)
                 pylab.annotate(note, (0.05, 0.9), xycoords='axes fraction',
