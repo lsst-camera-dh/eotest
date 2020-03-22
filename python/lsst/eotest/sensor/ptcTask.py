b4ef0be5f2a53102d4f663aebe522264b3a2909f
Seth Digel <digel@stanford.edu>
2020-03-22 12:24:11 -0700

    Updated variance and mean filtering in progress

--- python/lsst/eotest/sensor/ptcTask.py
+++ python/lsst/eotest/sensor/ptcTask.py
@@ -20,6 +20,8 @@ import lsst.eotest.image_utils as imutils
 from .MaskedCCD import MaskedCCD
 from .EOTestResults import EOTestResults
 from .flatPairTask import find_flat2
+import astropy.stats as astats
+import matplotlib.pyplot as plt
 
 def exptime(x): return imutils.Metadata(x).get('EXPTIME')
 
@@ -56,9 +58,11 @@ def residuals(pars, mean, var):
 
 
 class FlatPairStats(object):
-    def __init__(self, fmean, fvar):
+    def __init__(self, fmean, fvar, fmean_mad, fvar_mad):
         self.flat_mean = fmean
         self.flat_var = fvar
+        self.flat_mean_mad = fmean_mad
+        self.flat_var_mad = fvar_mad
 
 def flat_pair_stats(ccd1, ccd2, amp, mask_files=(), bias_frame=None):
     if ccd1.md.get('EXPTIME') != ccd2.md.get('EXPTIME'):
@@ -89,17 +93,92 @@ def flat_pair_stats(ccd1, ccd2, amp, mask_files=(), bias_frame=None):
         # Make a deep copy since otherwise the pixel values in image1
         # would be altered in the ratio calculation.
         #
-        fratio_im = afwImage.MaskedImageF(image1, True)
-        operator.itruediv(fratio_im, image2)
-        fratio = mean(fratio_im)
-        image2 *= fratio
+        #fratio_im = afwImage.MaskedImageF(image1, True)
+        #operator.itruediv(fratio_im, image2)
+        #fratio1 = mean(fratio_im)
+        fratio1 = mean(image1)/mean(image2)
+        #print('Before scaling')
+        #print('mean(image2):  ' + str(mean(image2)))
+        image2 *= fratio1
+        #print('After scaling')
+        #print('mean(image2):  ' + str(mean(image2)))
         fmean = (mean(image1) + mean(image2))/2.
 
-        fdiff = afwImage.MaskedImageF(image1, True)
-        fdiff -= image2
-        fvar = var(fdiff)/2.
+        fdiff1 = afwImage.MaskedImageF(image1, True)
+        fdiff1 -= image2
+        #print(amp,mean(image1),mean(image2),fmean, mean(fdiff))
+        fvar = var(fdiff1)/2.
 
-    return FlatPairStats(fmean, fvar)
+        #print(np.min(np.ravel(fdiff.getArrays()[0])), np.max(np.ravel(fdiff.getArrays()[0])))
+        #if amp == 7:
+            #fig,ax = plt.subplots()
+            #temp = plt.hist(np.ravel(fdiff.getArrays()[0]),bins=200,histtype='step',color='orange')
+            #plt.plot([-np.sqrt(fvar), -np.sqrt(fvar)],[0,np.max(temp[0])],color='blue')
+            #plt.plot([np.sqrt(fvar), np.sqrt(fvar)],[0,np.max(temp[0])],color='blue')
+        
+	# Make a more robust estimate of variance
+        fdiff = np.ravel(fdiff1.getArrays()[0])
+        fvar_mad1 = astats.mad_std(fdiff)  #/2.
+        image1 = np.ravel(image1.getArrays()[0])
+        #image2 = np.ravel(ccd2.unbiased_and_trimmed_image(amp, bias_frame=bias_frame).getArrays()[0])
+        image2 = np.ravel(image2.getArrays()[0])
+        #g = np.where((np.abs(fdiff) < (fvar_mad1*14.826))*(np.abs(image2) > 1e-3))[0]
+        #g1 = np.where((np.abs(image2) > 1e-3))[0]
+        g = np.where((np.abs(fdiff) < (fvar_mad1*14.826)))[0]
+        #if amp == 15:
+            #fig,ax = plt.subplots()
+            #plt.hist(fdiff,bins=200)
+            #print('Mean image1:  ' + str(mean(image1)))
+            #print('Mean image2:  ' + str(mean(image2)))
+
+        #fratio = np.mean(image1[g]/image2[g])
+        fratio = np.mean(image1[g])/np.mean(image2[g])
+        image2 *= fratio
+        fmean_mad = (np.mean(image1[g]) + np.mean(image2[g]))/2.
+        fdiff = image1[g] - image2[g]
+        fvar_mad = np.var(fdiff)/2.
+        print('len(g):  ' + str(len(g)))
+        if fmean_mad < 0:
+            print('!!!!!!!!!!! fmean_mad:  ' + str(fmean_mad))
+            print('len(g):  ' + str(len(g)))
+            #print('len(g1):  ' + str(len(g1)))
+            #print('len(g2):  ' + str(len(g2)))
+            print('fmean:  ' + str(fmean))
+            print('min,max fdiff:  ' + str(min(fdiff)) + ' ' + str(max(fdiff)))
+            print('min,max fdiff1:  ' + str(np.min(fdiff1.getArrays()[0])) + ' ' + str(np.max(fdiff1.getArrays()[0])))
+            print('str(type(fdiff1)):  ' + str(type(fdiff1)))
+            print('fmean_mad:  ' + str(fmean_mad))
+            print('np.mean(image1[g]):  ' + str(np.mean(image1[g])))
+            print('np.mean(image2[g]):  ' + str(np.mean(image2[g])))
+            print('np.mean(image1):  ' + str(np.mean(image1)))
+            print('np.mean(image2):  ' + str(np.mean(image2)))
+            print('fratio:  ' + str(fratio))
+            print('fratio1:  ' + str(fratio1))
+            print('fvar:  ' + str(fvar))
+            print('fvar_mad:  ' + str(fvar_mad))
+            print('fvar_mad1:  ' + str(fvar_mad1))
+            print('amp:  ' + str(amp))
+            print('mean(fdiff1):  ' + str(mean(fdiff1)))
+            print('mean(fdiff):  ' + str(mean(fdiff)))
+            print('files:  ')
+            print(ccd1.imfile)
+            print(ccd2.imfile)
+
+
+        #if amp == 7:
+            #temp = plt.hist(np.ravel(fdiff),bins=200,color='green',histtype='step')
+            #plt.plot([-np.sqrt(fvar_mad), -np.sqrt(fvar_mad)],[0,np.max(temp[0])],color='red')
+            #plt.plot([np.sqrt(fvar_mad), np.sqrt(fvar_mad)],[0,np.max(temp[0])],color='red')
+            #plt.title('AMP' + str(amp).zfill(2))
+            #outfile = 'temp_png/hist_' + ccd1.imfile.split('/')[-1].split('.')[0] + '.png'
+            #print('Saving image file:  ' + outfile)
+            #plt.savefig(outfile)
+
+        #print('fmean:  ' + str(fmean))
+        #print('fvar:  ' + str(fvar))
+        #print('fmean_mad:  ' + str(fmean_mad))
+        #print('fvar_mad:  ' + str(fvar_mad))
+    return FlatPairStats(fmean, fvar, fmean_mad, fvar_mad)
 
 
 class PtcConfig(pexConfig.Config):
@@ -124,33 +203,47 @@ class PtcTask(pipeBase.Task):
         outfile = os.path.join(self.config.output_dir,
                                '%s_ptc.fits' % sensor_id)
         all_amps = imutils.allAmps(infiles[0])
-        ptc_stats = dict([(amp, ([], [])) for amp in all_amps])
+        #print(all_amps)
+        ptc_stats = dict([(amp, ([], [], [], [])) for amp in all_amps])
         exposure = []
         seqnums = []
         dayobs = []
-        file1s = sorted([item for item in infiles if item.find('flat1') != -1])
+        file1s = sorted([item for item in infiles if item.find('flat0') != -1])
+        print('# file1s:  ' + str(len(file1s)))
         for flat1 in file1s:
             flat2 = flat2_finder(flat1)
             if self.config.verbose:
                 self.log.info("processing %s" % flat1)
             exposure.append(exptime(flat1))
+            print('Reading ccd1')
+            print(flat1)
             ccd1 = MaskedCCD(flat1, mask_files=mask_files,
                              bias_frame=bias_frame,
                              linearity_correction=linearity_correction)
+            #ccd1.stat_ctrl.setNumIter(3)
+            #ccd1.stat_ctrl.setNumSigmaClip(10.0)
+            print('Reading ccd2')
+            print(flat2)
             ccd2 = MaskedCCD(flat2, mask_files=mask_files,
                              bias_frame=bias_frame,
                              linearity_correction=linearity_correction)
+            #ccd2.stat_ctrl.setNumIter(3)
+            #ccd2.stat_ctrl.setNumSigmaClip(10.0)
+            #print(len(ccd1), len(ccd2))
             for amp in ccd1:
                 results = flat_pair_stats(ccd1, ccd2, amp,
                                           mask_files=mask_files,
                                           bias_frame=bias_frame)
                 ptc_stats[amp][0].append(results.flat_mean)
                 ptc_stats[amp][1].append(results.flat_var)
+                ptc_stats[amp][2].append(results.flat_mean_mad)
+                ptc_stats[amp][3].append(results.flat_var_mad)
             seqnums.append(ccd1.md.get('SEQNUM'))
             try:
                 dayobs.append(ccd1.md.get('DAYOBS'))
             except KeyError:
                 dayobs.append(0)
+        print('Now fitting the curves')
         self._fit_curves(ptc_stats, sensor_id)
         output = fits.HDUList()
         output.append(fits.PrimaryHDU())
@@ -158,17 +251,25 @@ class PtcTask(pipeBase.Task):
         units = ['seconds']
         columns = [np.array(exposure, dtype=np.float)]
         for amp in all_amps:
-            colnames.extend(['AMP%02i_MEAN' % amp, 'AMP%02i_VAR' % amp])
-            units.extend(['ADU', 'ADU**2'])
+            colnames.extend(['AMP%02i_MEAN' % amp, 'AMP%02i_VAR' % amp, 'AMP%02i_MMEAN' % amp, 'AMP%02i_MVAR' % amp])
+            units.extend(['ADU', 'ADU**2', 'ADU', 'ADU**2'])
             columns.extend([np.array(ptc_stats[amp][0], dtype=np.float),
-                            np.array(ptc_stats[amp][1], dtype=np.float)])
+                            np.array(ptc_stats[amp][1], dtype=np.float),
+                            np.array(ptc_stats[amp][2], dtype=np.float),
+                            np.array(ptc_stats[amp][3], dtype=np.float)])
         colnames.append('SEQNUM')
         units.append('None')
         columns.append(seqnums)
         colnames.append('DAYOBS')
         units.append('None')
         columns.append(dayobs)
-        formats = 'E'*(len(colnames) - 2) + 'JJ'
+        colnames.append('FILE0')
+        units.append('None')
+        columns.append(ccd1.imfile.split('/')[-1])
+        colnames.append('FILE1')
+        units.append('None')
+        columns.append(ccd2.imfile.split('/')[-1])
+        formats = 'E'*(len(colnames) - 2) + 2*'J' + 2*'A'
         fits_cols = [fits.Column(name=colnames[i], format=formats[i],
                                  unit=units[i], array=columns[i])
                      for i in range(len(columns))]
@@ -233,6 +334,7 @@ class PtcTask(pipeBase.Task):
         if outfile is None:
             outfile = os.path.join(self.config.output_dir,
                                    '%s_eotest_results.fits' % sensor_id)
+        print('Writing:  ' + outfile)
         output = EOTestResults(outfile, namps=len(ptc_stats))
         # Fit for gain and error, a00 and its uncertainty, inferred
         # noise and uncertainty, and the 'turnoff' level (in
@@ -249,6 +351,25 @@ class PtcTask(pipeBase.Task):
             output.add_seg_result(amp, 'PTC_NOISE', ptc_noise)
             output.add_seg_result(amp, 'PTC_NOISE_ERROR', ptc_noise_error)
             output.add_seg_result(amp, 'PTC_TURNOFF', ptc_turnoff)
+            self.log.info("%i  %f  %f %f %f %f %f %f" % (amp, ptc_gain,
+                                                         ptc_error, ptc_a00,
+                                                         ptc_a00_error,
+                                                         ptc_noise,
+                                                         ptc_noise_error,
+                                                         ptc_turnoff))
+
+            # Now fit the means and variances that were evaluated with MAD filtering
+            mean, var = np.array(ptc_stats[amp][2]), np.array(ptc_stats[amp][3])
+            (ptc_gain, ptc_error, ptc_a00, ptc_a00_error, ptc_noise,
+             ptc_noise_error, ptc_turnoff) \
+             = self.fit_ptc_curve(mean, var, sig_cut=sig_cut, logger=self.log)
+            output.add_seg_result(amp, 'PTC_MGAIN', ptc_gain)
+            output.add_seg_result(amp, 'PTC_MGAIN_ERROR', ptc_error)
+            output.add_seg_result(amp, 'PTC_MA00', ptc_a00)
+            output.add_seg_result(amp, 'PTC_MA00_ERROR', ptc_a00_error)
+            output.add_seg_result(amp, 'PTC_MNOISE', ptc_noise)
+            output.add_seg_result(amp, 'PTC_MNOISE_ERROR', ptc_noise_error)
+            output.add_seg_result(amp, 'PTC_MTURNOFF', ptc_turnoff)
             self.log.info("%i  %f  %f %f %f %f %f %f" % (amp, ptc_gain,
                                                          ptc_error, ptc_a00,
                                                          ptc_a00_error,
